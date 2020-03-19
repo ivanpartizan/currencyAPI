07764923c9ce30e00c94c24bd11097c1ad9af6d5
Ivan Milenkovic <argentinaivan@hotmail.com>
2020-03-20 00:10:25 +0100

    update

--- script.js
+++ script.js
@@ -17,58 +17,70 @@ async function currencyConverter() {
   const data = await response.json();
   console.log(data);
 
-  document.getElementById("output").innerHTML = `
-  <div class='usa'>
-  <h2>USD</h2> <img src='usd.svg' height='40'> <h2>${(
-    input.value * data.rates.USD
-  ).toFixed(2)}</h2>
-  </div>
-  <div class='cad'>
-  <h2>CAD</h2> <img src='cad.svg' height='40'> <h2>${(
-    input.value * data.rates.CAD
-  ).toFixed(2)}</h2>
-  </div>
-  <div class='aud'>
-  <h2>AUD</h2> <img src='aud.svg' height='40'> <h2>${(
-    input.value * data.rates.AUD
-  ).toFixed(2)}</h2>
-  </div>
-  <div class='chf'>
-  <h2>CHF</h2> <img src='chf.svg' height='40'> <h2>${(
-    input.value * data.rates.CHF
-  ).toFixed(2)}</h2>
-  </div>
-  <div class='gbp'>
-  <h2>GBP</h2> <img src='gbp.svg' height='40'> <h2>${(
-    input.value * data.rates.GBP
-  ).toFixed(2)}</h2>
-  </div>
-  <div class='jpy'>
-  <h2>JPY</h2> <img src='jpy.svg' height='40'> <h2>${(
-    input.value * data.rates.JPY
-  ).toFixed(2)}</h2>
-  </div>
-  <div class='rub'>
-  <h2>RUB</h2> <img src='rub.svg' height='40'> <h2>${(
-    input.value * data.rates.RUB
-  ).toFixed(2)}</h2>
-  </div>
-  <div class='rsd'>
-  <h2>RSD</h2> <img src='rsd.svg' height='40'> <h2>${(
-    input.value * data.rates.RSD
-  ).toFixed(2)}</h2>
-  </div>
-  <div class='brl'>
-  <h2>BRL</h2> <img src='brl.svg' height='40'> <h2>${(
-    input.value * data.rates.BRL
-  ).toFixed(2)}</h2>
-  </div>
-  <div class='ars'>
-  <h2>ARS</h2> <img src='ars.svg' height='40'> <h2>${(
-    input.value * data.rates.ARS
-  ).toFixed(2)}</h2>
-  </div>
+  if (input.value >= 0) {
+    document.getElementById("output").innerHTML = `
+    <div class='usa'>
+    <h2>USD</h2> <img src='usd.svg' height='40'> <h2>${(
+      (input.value * data.rates.USD) /
+      input.value
+    ).toFixed(2)}</h2>
+    </div>
+    <div class='cad'>
+    <h2>CAD</h2> <img src='cad.svg' height='40'> <h2>${(
+      (input.value * data.rates.CAD) /
+      input.value
+    ).toFixed(2)}</h2>
+    </div>
+    <div class='aud'>
+    <h2>AUD</h2> <img src='aud.svg' height='40'> <h2>${(
+      (input.value * data.rates.AUD) /
+      input.value
+    ).toFixed(2)}</h2>
+    </div>
+    <div class='chf'>
+    <h2>CHF</h2> <img src='chf.svg' height='40'> <h2>${(
+      (input.value * data.rates.CHF) /
+      input.value
+    ).toFixed(2)}</h2>
+    </div>
+    <div class='gbp'>
+    <h2>GBP</h2> <img src='gbp.svg' height='40'> <h2>${(
+      (input.value * data.rates.GBP) /
+      input.value
+    ).toFixed(2)}</h2>
+    </div>
+    <div class='jpy'>
+    <h2>JPY</h2> <img src='jpy.svg' height='40'> <h2>${(
+      (input.value * data.rates.JPY) /
+      input.value
+    ).toFixed(2)}</h2>
+    </div>
+    <div class='rub'>
+    <h2>RUB</h2> <img src='rub.svg' height='40'> <h2>${(
+      (input.value * data.rates.RUB) /
+      input.value
+    ).toFixed(2)}</h2>
+    </div>
+    <div class='rsd'>
+    <h2>RSD</h2> <img src='rsd.svg' height='40'> <h2>${(
+      (input.value * data.rates.RSD) /
+      input.value
+    ).toFixed(2)}</h2>
+    </div>
+    <div class='brl'>
+    <h2>BRL</h2> <img src='brl.svg' height='40'> <h2>${(
+      (input.value * data.rates.BRL) /
+      input.value
+    ).toFixed(2)}</h2>
+    </div>
+    <div class='ars'>
+    <h2>ARS</h2> <img src='ars.svg' height='40'> <h2>${(
+      (input.value * data.rates.ARS) /
+      input.value
+    ).toFixed(2)}</h2>
+    </div>
   `;
+  }
 }
 
 currencyConverter();
